0f2ec0ccae91a65c45aefab654473017da1cb5f2
Ethan <ethan.m.harry@gmail.com>
2016-11-02 11:53:25 -0700

    Finished all but last requirement

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,16 +4,22 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   var size = 0;
+  var i = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[i] = value; // Storage: {0: a} -- Storage: {}
+
     size++;
   };
 
   someInstance.pop = function() {
+
     if (size > 0) {
     size--;
     }
+
+    return storage[i];
 
   };
 
