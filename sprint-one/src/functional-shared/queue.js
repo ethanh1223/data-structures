d7565d22f49660b4429c8073829c750cf898abca
Bruce Paul <brucewpaul@gmail.com>
2016-11-02 12:12:23 -0700

    Pass functional shared test: "reports a size of zero for a new queue"

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,18 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {}
+  newQueue.storage = {}
+  newQueue.currentSize = 0;
+
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
+
 };
 
 var queueMethods = {};
 
-
+queueMethods.size = function() {
+  return this.currentSize;
+}
