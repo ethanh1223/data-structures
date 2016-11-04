e1810ebda02900e3516a9c10dcaca424f6aefcb6
ethanh1223 <ethan.m.harry@gmail.com>
2016-11-03 19:10:27 -0700

    Finish Tree Data Structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,20 +15,33 @@ treeMethods.addChild = function(value) {
   var childTree = Tree(value);
   // Push tree to children (array)
   this.children.push(childTree);
-  // returned this obj gt
+  // returned this obj gt 
   return childTree;
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, node) {
+  // check if this.value is equal to target
+  node = node || this;
+  
+  if ( node.value === target ) {
+    // if yes return true
+    return true;
+  } 
+
+  // for loop through this.children
+  for ( var j = 0; j < node.children.length; j++ ) {
+    // recursively call this.contains
+    debugger;
+    var isHere = node.contains(target, node.children[j]);
+    debugger;
+    if ( isHere === true ) {
+      return true;
+    }
+
+  }
+  debugger;
+  return false;
 };
-
-// var familyTree = Tree('dad');
-
-// var son = familyTree.addChild('son');
-
-// son.addChild
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
