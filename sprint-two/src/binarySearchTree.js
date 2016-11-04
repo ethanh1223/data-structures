cd304999c537cb8d6b4dac4912d27a82cfe82031
ethanh1223 <ethan.m.harry@gmail.com>
2016-11-04 16:51:17 -0700

    Start BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,46 @@
 var BinarySearchTree = function(value) {
-};
+  var node = {};
 
+  node.value = value;
+
+  node.left = {};
+  node.right = {};
+
+  // insert(value)
+  node.insert = function(value) {
+
+    // if value less than node.value
+    if (value < node.value) {
+      // if there is already a node.left
+      if (Object.keys(node.left).length > 0) {
+        // node.left.insert new BST of value
+        node.left.insert(value);
+      } else {
+        // node.left is a new BST of value
+        node.left = BinarySearchTree(value);
+      }
+
+      
+    }
+      
+    // else
+      // if there is already a node.right
+        // add new BST of value to node.right
+      // else
+        // node.right is a new BST of value
+  };
+    
+
+  // contains
+
+  // depthFirstLog
+
+
+  return node;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var numbers = BinarySearchTree(5);
