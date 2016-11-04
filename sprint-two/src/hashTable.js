3f439eb2e23c330d01fe708f1cbf9fe697e07607
ethanh1223 <ethan.m.harry@gmail.com>
2016-11-04 11:27:37 -0700

    Work on Hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,17 +7,22 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit); 
+  this._storage.each(function(e, i, a) {
+    if ( i === index ) {
+      a[i] = undefined;
+    }
+  });
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
